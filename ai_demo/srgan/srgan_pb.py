d4928cacfe4bafa017b244a174b4fbed88215a5f
zhouxiangjing <zhouxiangjing2018@163.com>
2019-04-29 11:15:24 +0800

    修改srgan模型

--- ai_demo/srgan/srgan_pb.py
+++ ai_demo/srgan/srgan_pb.py
@@ -5,11 +5,39 @@ from tensorflow.python.platform import gfile
 import tensorlayer as tl
 from PIL import Image
 import numpy as np
-import scipy.misc as sic
+import scipy.misc
 
 pb_file = './models/srgan.pb'
-
 image_lr_file = './images/img_001.png'
+
+def get_images(filename, is_crop, fine_size, images_norm):
+    img = scipy.misc.imread(filename, mode='RGB')
+    if is_crop:
+        size = img.shape
+        start_h = int((size[0] - fine_size)/2)
+        start_w = int((size[1] - fine_size)/2)
+        img = img[start_h:start_h+fine_size, start_w:start_w+fine_size,:]
+    img = np.array(img).astype(np.float32)
+    if images_norm:
+        img = (img-127.5)/127.5
+    return img
+
+
+def save_images(images, size, filename):
+    return scipy.misc.imsave(filename, merge_images(images, size))
+
+
+def merge_images(images, size):
+    h, w = images.shape[1], images.shape[2]
+    imgs = np.zeros((size[0] * h, size[1] * w, 3))
+
+    for index, image in enumerate(images):
+        i = index // size[1]
+        j = index % size[0]
+        imgs[i * h:i * h + h, j * w:j * w + w, :] = image
+
+    return imgs
+
 
 with tf.Session() as sess:
     with gfile.FastGFile(pb_file, 'rb') as f:  # 加载模型
@@ -18,15 +46,13 @@ with tf.Session() as sess:
         sess.graph.as_default()
         tf.import_graph_def(graph_def, name='')  # 导入计算图
 
-    input_tensor = sess.graph.get_tensor_by_name('inputs_raw:0')
-    output_tensor = sess.graph.get_tensor_by_name('encode_image/output_pngs/TensorArrayStack/TensorArrayGatherV3:0')
+    input_tensor = sess.graph.get_tensor_by_name('input_target:0')
+    output_tensor = sess.graph.get_tensor_by_name('generator/Tanh:0')
 
-    image_lr_data = sic.imread(image_lr_file, mode="RGB").astype(np.float32)
-    image_lr_data = image_lr_data / np.max(image_lr_data)
-    input_im = np.array([image_lr_data]).astype(np.float32)
+    batch_x = [get_images('./images/baboon.bmp', True, 256, True)]
+    batchs = np.array(batch_x).astype(np.float32)
 
-    results = sess.run(output_tensor, feed_dict={input_tensor: input_im})
+    results = sess.run(output_tensor, feed_dict={input_tensor: batchs})
 
-    contents = results[0]
-    with open('./images/img_001-outputs.png', "wb") as f:
-        f.write(contents)
+    results = np.expand_dims(results[0], 0)
+    save_images(results, [1, 1], './images/output.png')
--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 
+import tensorflow as tf;
+import numpy as np;
 
+A = np.array([[1, 2, 3, 11],
+			  [4, 5, 6, 12],
+			  [7, 8, 9, 13]])
 
-def fib(max):
-    n, a, b = 0, 0, 1
-    while n < max:
-        print(b)
-        a, b = b, a + b
-        n = n + 1
+x = tf.transpose(A, [1 ,0])
 
+B = np.array([[[1, 2 ,3],
+			   [4 ,5 ,6]]])
 
-fib(20)
+y = tf.transpose(B, [2 ,1 ,0])
+
+with tf.Session() as sess:
+	print(A[1 ,0])
+	print(sess.run(x))
+	print(B[0 ,1 ,2])
+	print(sess.run(y[2 ,1 ,0]))
