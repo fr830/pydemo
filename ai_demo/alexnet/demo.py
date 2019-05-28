f371b65e6cae447aa26ad34d22b8341810fb2e79
zhouxiangjing <zhouxiangjing2018@163.com>
2019-05-28 10:39:25 +0800

    更新

--- ai_demo/alexnet/demo.py
+++ ai_demo/alexnet/demo.py
@@ -2,67 +2,10 @@
 import tensorflow as tf
 from alexnet import AlexNet
 
-#
-# train_file = 'D:/tensorflow/bvlc_alexnet/finetune_alexnet_with_tensorflow-master\train.txt'
-# val_file = 'D:/tensorflow\bvlc_alexnet/finetune_alexnet_with_tensorflow-master\val.txt'
-#
-# with tf.device('/cpu:0'):
-#     tr_data = ImageDataGenerator(filed,
-#                                  train_file,
-#                                  mode='training',
-#                                  batch_size=batch_size,
-#                                  num_classes=num_classes,
-#                                  shuffle=True)
-#
-#     val_data = ImageDataGenerator(filed,
-#                                   val_file,
-#                                   mode='inference',
-#                                   batch_size=batch_size,
-#                                   num_classes=num_classes,
-#                                   shuffle=False)
-#
-#     # create an reinitializable iterator given the dataset structure
-#     iterator = Iterator.from_structure(tr_data.data.output_types, tr_data.data.output_shapes)
-#     next_batch = iterator.get_next()
-#
-# x = tf.placeholder(tf.float32, [batch_size, 227, 227, 3], name='input')
-# y = tf.placeholder(tf.float32, [batch_size, num_classes], name='y')
-# keep_prob = tf.placeholder(tf.float32)
-#
-# model = AlexNet(x, keep_prob, num_classes, train_layers)
-#
-# score = model.fc8
-#
-# var_list = [v for v in tf.trainable_variables() if v.name.split('/')[0] in train_layers]
-#
-# with tf.name_scope("cross_ent"):
-#     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=score, labels=y))
-#
-# # Train op
-# with tf.name_scope("train"):
-#     # Get gradients of all trainable variables
-#     gradients = tf.gradients(loss, var_list)
-#     gradients = list(zip(gradients, var_list))
-#
-#     # Create optimizer and apply gradient descent to the trainable variables
-#     optimizer = tf.train.GradientDescentOptimizer(learning_rate)
-#     train_op = optimizer.apply_gradients(grads_and_vars=gradients)
-#
-#
-# with tf.name_scope("accuracy"):
-#     correct_pred = tf.equal(tf.argmax(score, 1), tf.argmax(y, 1))
-#     accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
-#
-# with tf.Session() as sess:
-#     sess.run(tf.global_variables_initializer())
-#
-#     for epoch in range(num_epochs):
-#         img_batch, label_batch = sess.run(next_batch)
-#         acc = sess.run(accuracy, feed_dict={x: img_batch, y: label_batch, keep_prob: 1.})
-
 from datagenerator import *
-import random
+import os, random
 from datetime import datetime
+import numpy as np
 
 # 加载数据
 train_data, test_data = load_data()
@@ -70,7 +13,7 @@ train_data, test_data = load_data()
 # 定义网络超参数
 learning_rate = 1e-4  # 学习率
 training_epoches = 40  # 训练轮数
-batch_size = 512  # 小批量大小
+batch_size = 256  # 小批量大小
 num_classes = 10
 train_layers = ['fc8', 'fc7', 'fc6']
 n_train = len(train_data)  # 训练集数据长度
@@ -87,19 +30,25 @@ inference = model.fc8
 
 with tf.name_scope('loss'):
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=inference, labels=input_y))
+    tf.summary.scalar('loss', loss)
 
 with tf.name_scope('accuracy'):
     correct = tf.equal(tf.argmax(inference, 1), tf.argmax(input_y, 1))
     accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
+    tf.summary.scalar('accuracy', accuracy)
 
 saver = tf.train.Saver()
 
 train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
+
 config = tf.ConfigProto()
 config.gpu_options.allow_growth = True
 with tf.Session(config=config) as sess:
     sess.run(tf.global_variables_initializer())
+
+    merged = tf.summary.merge_all()  # 将图形、训练过程等数据合并在一起
+    writer = tf.summary.FileWriter('./log', sess.graph)  # 将训练日志写入到logs文件夹下
 
     step = 0
     for i in range(training_epoches):
@@ -116,6 +65,9 @@ with tf.Session(config=config) as sess:
             use_time = (datetime.now()-start_time).seconds
             print('step {0}/{1} | {2}/{3} {4}s: training set accuracy : {5}, loss : {6}'.format(i, training_epoches, j, len(train_batchs), use_time, np.mean(train_accuracy), np.mean(train_loss)))
 
+            result1 = sess.run(merged, feed_dict={input_x : batch_x, input_y : batch_y})
+            writer.add_summary(result1, i*len(train_batchs) + j)
+
         # 测试
         test_batchs = [test_data[k:k + batch_size] for k in range(0, n_test, batch_size)]
         test_batchs.pop()
@@ -123,9 +75,9 @@ with tf.Session(config=config) as sess:
         test_cost_sum = []
         for mini_batch in test_batchs:
             batch_x, batch_y = get_image_data_and_label(mini_batch, image_size=image_size, one_hot=True)
-            test_accuracy, test_cost = sess.run([accuracy, loss], feed_dict={input_x: batch_x, input_y: batch_y})
+            test_accuracy, test_loss = sess.run([accuracy, loss], feed_dict={input_x: batch_x, input_y: batch_y})
             test_accuracy_sum.append(test_accuracy)
-            test_cost_sum.append(test_cost)
+            test_cost_sum.append(test_loss)
         print('Epoch {0}: Test set accuracy {1}, loss {2}.'.format(i, np.mean(test_accuracy_sum), np.mean(test_cost_sum)))
 
         saver.save(sess, os.path.join('./models', 'alexnet'), global_step=len(train_batchs)*i)
--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 import tensorflow as tf;
 import numpy as np;
 
-from datetime import datetime
-
-a=datetime.now()
-b=datetime.now()
-
-bb = (b-a).seconds
-
-nnn = -1;
+import tensorflow as tf
+a = tf.constant([1.0,2.0,3.0],name='input1')
+b = tf.Variable(tf.random_uniform([3]),name='input2')
+add = tf.add_n([a,b],name='addOP')
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    writer = tf.summary.FileWriter("C:/Users/86529/Desktop/alexnet",sess.graph)
+    print(sess.run(add))
