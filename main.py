b06a98effeb8098c4cf92a14cdfdde9b6a7a5edc
zhouxiangjing <zhouxiangjing2018@163.com>
2019-06-04 14:05:27 +0800

    更新ai

--- main.py
+++ main.py
@@ -2,11 +2,21 @@
 import tensorflow as tf;
 import numpy as np;
 
-import tensorflow as tf
-a = tf.constant([1.0,2.0,3.0],name='input1')
-b = tf.Variable(tf.random_uniform([3]),name='input2')
-add = tf.add_n([a,b],name='addOP')
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    writer = tf.summary.FileWriter("C:/Users/86529/Desktop/alexnet",sess.graph)
-    print(sess.run(add))
+class SquareTest(tf.test.TestCase):
+
+    def testSquare1(self):
+        with self.session():
+            # 平方操作
+            x = tf.square([2, 3])
+            # 测试x的值是否等于[4,9]
+            self.assertAllEqual(x.eval(), [4, 9])
+
+    def testSquare2(self):
+        with self.session():
+            # 平方操作
+            x = tf.square([2, 3])
+            # 测试x的值是否等于[4,9]
+            self.assertAllEqual(x.eval(), [4, 9])
+
+if __name__ == "__main__":
+    tf.test.main()
